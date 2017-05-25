4c803cb9df3e1d11228152a2c7cc85a572bc542c
James Wright <james.wright@red-gate.com>
2017-05-25 16:41:42 +0100

    Add console app

---
+++ tddkata4.Console/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace tddkata4.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var playerOne = new ConsolePlayer();
+            var playerTwo = new ConsolePlayer();
+
+            var game = new Game(playerOne, playerTwo);
+            while (true)
+            {
+                game.PlayRound();
+                System.Console.WriteLine(game.Winner);
+            }
+        }
+    }
+
+    internal class ConsolePlayer : IPlayer
+    {
+        public PlayerSelection Reveal()
+        {
+            
+            var input = System.Console.ReadLine();
+            switch (input)
+            {
+                case "rock":
+                    return PlayerSelection.Rock;
+                case "scissors":
+                    return PlayerSelection.Scissors;
+                case "paper":
+                    return PlayerSelection.Paper;
+                default:
+                    System.Console.WriteLine("Try again");
+                    return Reveal();
+            }
+        }
+    }
+}
