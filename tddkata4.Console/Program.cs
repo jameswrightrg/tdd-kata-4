d8ed05766eb498f7c9371f662fa2a22104ee5aa5
James Wright <james.wright@red-gate.com>
2017-05-25 16:45:03 +0100

    Improve interface

--- tddkata4.Console/Program.cs
+++ tddkata4.Console/Program.cs
@@ -4,31 +4,44 @@
     {
         static void Main(string[] args)
         {
-            var playerOne = new ConsolePlayer();
-            var playerTwo = new ConsolePlayer();
+            var playerOne = new ConsolePlayer(1);
+            var playerTwo = new ConsolePlayer(2);
 
             var game = new Game(playerOne, playerTwo);
+            var round = 1;
             while (true)
             {
+                System.Console.WriteLine($"Round {round++}!");
                 game.PlayRound();
-                System.Console.WriteLine(game.Winner);
+                System.Console.WriteLine($"Winner: {game.Winner}");
+                System.Console.WriteLine();
             }
         }
     }
 
     internal class ConsolePlayer : IPlayer
     {
+        private readonly int m_PlayerNumber;
+
+        public ConsolePlayer(int playerNumber)
+        {
+            m_PlayerNumber = playerNumber;
+        }
+
         public PlayerSelection Reveal()
         {
-            
+            System.Console.WriteLine($"Player {m_PlayerNumber}, enter your selection:");
             var input = System.Console.ReadLine();
             switch (input)
             {
                 case "rock":
+                case "r":
                     return PlayerSelection.Rock;
                 case "scissors":
+                case "s":
                     return PlayerSelection.Scissors;
                 case "paper":
+                case "p":
                     return PlayerSelection.Paper;
                 default:
                     System.Console.WriteLine("Try again");
