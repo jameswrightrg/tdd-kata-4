25cef528def6336c7aec3709effde9688e1d1f83
James Wright <james.wright@red-gate.com>
2017-05-25 10:39:19 +0100

    Refactor repeated code

--- tddkata4/Round.cs
+++ tddkata4/Round.cs
@@ -4,6 +4,10 @@
     {
         public static Winner Winner(PlayerSelection playerOne, PlayerSelection playerTwo)
         {
+            if (playerOne == playerTwo)
+            {
+                return tddkata4.Winner.Draw;
+            }
             if (playerOne == PlayerSelection.Scissors && playerTwo == PlayerSelection.Rock)
             {
                 return tddkata4.Winner.PlayerTwo;
@@ -15,18 +19,6 @@
             if (playerOne == PlayerSelection.Rock && playerTwo == PlayerSelection.Paper)
             {
                 return tddkata4.Winner.PlayerTwo;
-            }
-            if (playerOne == PlayerSelection.Rock && playerTwo == PlayerSelection.Rock)
-            {
-                return tddkata4.Winner.Draw;
-            }
-            if (playerOne == PlayerSelection.Scissors && playerTwo == PlayerSelection.Scissors)
-            {
-                return tddkata4.Winner.Draw;
-            }
-            if (playerOne == PlayerSelection.Paper && playerTwo == PlayerSelection.Paper)
-            {
-                return tddkata4.Winner.Draw;
             }
             return tddkata4.Winner.PlayerOne;
         }
